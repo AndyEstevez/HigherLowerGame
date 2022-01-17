ea0bfe620af1066e06b7f5725c1cc2a6c179883f
unknown <andyestevez27@gmail.com>
2022-01-17 13:06:00 -0500

    Initial frontend install

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    IN HOME PAGE      
+    </div>
+  );
+}
+
+export default App;
