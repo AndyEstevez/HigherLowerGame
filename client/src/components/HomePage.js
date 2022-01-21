f5245e58746e432c07b0248e00301727828d00e5
unknown <andyestevez27@gmail.com>
2022-01-21 11:23:11 -0500

    updated responsive css

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,18 +3,18 @@ import background from '../bg-image.jpg'
 
 const HomePage = () => {
     return (        
-            <div>
+            <div >
                 <div style={{ position: "relative", width: "100%", height: "100vh", display: "flex", 
                             backgroundImage: `url(${background})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
                     <div class="container text-center justify-content-center align-items-center" 
-                        style={{fontFamily: "Tiempos Headline", color: "white", fontSize: "2em", fontWeight: "600", margin: "auto"}}>
-                        <h1 style={{ fontSize: "4em"}}>Higher Lower Game</h1>
-                        <p>A game of higher or lower based on Pitchfork scores.</p>
-                        <p>Currently uses albums released in 2020 and 2021.</p>
+                        style={{fontFamily: "Tiempos Headline", color: "white", fontSize: "5vw", fontWeight: "600", margin: "auto"}}>
+                        <h1 style={{ fontSize: "8vw"}}>Higher Lower Game</h1>
+                        <p style={{ fontSize: "3vw"}}>A game of higher or lower based on Pitchfork scores.</p>
+                        <p style={{ fontSize: "3vw"}}>Currently uses albums released in 2020 and 2021.</p>
                         <Link to={'/game'} style={{textDecoration: "none"}}>
                             <button type="button" 
                                 class="btn btn-success btn-lg" 
-                                style={{margin: "auto"}}>
+                                style={{margin: "auto", fontSize: "3vw"}}>
                                 Start Game
                             </button>
                         </Link>
